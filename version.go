06ce164f7e5f78254a1aedd47f27b7cccb4b2c18
Вячеслав Бойко <boyko@bradburylab.com>
2018-05-14 17:38:31 +0300

    add Marshal interfaces for JSON and YAML

--- version.go
+++ version.go
@@ -167,17 +167,15 @@ func (v *Version) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	return nil
 }
 
-// UnmarshalJSON implements the json.Unmarshaler interface
-// Unmarshals a string into a Version
-func (v *Version) UnmarshalJSON(data []byte) error {
-	ver, err := NewVersion(string(data))
-	if err != nil {
-		return err
-	}
-
-	*v = *ver
-	return nil
+// MarshalYAML implements the yaml.Marshaler interface
+// Marshals a Version into a string
+func (v *Version) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("\"%s\"", v.String())), nil
 }
+
+// MarshalJSON implements the json.Marshaler interface
+// Marshals a Version into a string
+func (v *Version) MarshalYAML() (interface{}, error) { return v.String(), nil }
 
 func allZero(segs []int) bool {
 	for _, s := range segs {
